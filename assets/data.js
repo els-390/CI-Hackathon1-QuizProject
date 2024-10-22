a1a0ad90a725727b2337f972c2e42b3d7595cb49
(Philip) Luke <luke.isotta@gmail.com>
2024-10-22 00:47:09 +0000

    Populate: Cornwall question objects

--- assets/data.js
+++ assets/data.js
@@ -5,64 +5,64 @@ const questions = [
         questions: [
             {
                 // Question 1
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "What is the name of the capital city of Cornwall?",
+                options: ["Penzance", "Truro", "St Ives"],
+                answer: 2,      // refers to the index of the correct option
             },
             {
                 // Question 2
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "Which of the following beaches is located near Falmouth, Cornwall?",
+                options: ["Perranporth Beach", "Watergate Bay", "Gyllyngvase Beach"],
+                answer: 2,
             },
             {
                 // Question 3
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "Who was a famous pirate from Cornwall?",
+                options: ["Blackbeard", "Sir Francis Drake", "John Hawkins"],
+                answer: 1,
             },
             {
                 // Question 4
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "What is the name of the largest prehistoric monument in Cornwall?",
+                options: ["The Hurlers", "The Merry Maidens", "Stonehenge"],
+                answer: 0,
             },
             {
                 // Question 5
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "Which of the following Cornwall towns is known for its Eden Project?",
+                options: ["St Austell", "Truro", "Newquay"],
+                answer: 0,
             },
             {
                 // Question 6
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "Who was a famous artist from Newlyn, Cornwall?",
+                options: ["Jogn Singer Sargent", "Walter Sickert", "Laura Knight"],
+                answer: 2,
             },
             {
                 // Question 7
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "What is the name of the highest point in Cornwall?",
+                options: ["Brown Willy", "Carn Menellis", "The Hurlers"],
+                answer: 0,
             },
             {
                 // Question 8
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "Which of the following harbours is located near St Ives, Cornwall?",
+                options: ["Falmouth Harbour", "Mousehole Harbour", "St Ives Harbour"],
+                answer: 2,
             },
             {
                 // Question 9
-                question:
-                options: ["", "", ""],
-                answer:
+                question: "Who was a famous author from Cornwall?",
+                options: ["Daphne du Maurier", "Rosamunde Pilcher", "Helen Dunmore"],
+                answer: 0,
             },
             {
                 // Question 10
-                question:
-                options: ["", "", ""],
-                answer:
-            },
+                question: "What is the name of the River that flows through Truro, Cornwall?",
+                options: ["Fowey River", "Tamar River", "Kenwyn River"],
+                answer: 0
+            }
         ]
     }
 ]
