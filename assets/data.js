c974e16d4a26a219afdc865418839c218b7e9ed0
(Philip) Luke <luke.isotta@gmail.com>
2024-10-22 00:51:29 +0000

    Add: required number of question object templates to pop music topic

--- assets/data.js
+++ assets/data.js
@@ -70,11 +70,65 @@ const questions = [
         topic: "Pop-music trivia",
         questions: [
             {
-                // Question
+                // Question 1
                 question: ,
                 options: ["", "", ""],
                 answer: ,
-            }
+            },
+            {
+                // Question 2
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 3
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 4
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 5
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 6
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 7
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 8
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 9
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
+            {
+                // Question 10
+                question: ,
+                options: ["", "", ""],
+                answer: ,
+            },
         ]
     }
 ]
