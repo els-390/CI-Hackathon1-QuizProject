dca3ca945f6bd7c831b759ee0a63e3b50aeb28a7
(Philip) Luke <luke.isotta@gmail.com>
2024-10-22 00:43:23 +0000

    Add: required question templates to data.js cornwall

--- assets/data.js
+++ assets/data.js
@@ -4,7 +4,61 @@ const questions = [
         topic: "Cornwall",
         questions: [
             {
-                // Question
+                // Question 1
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 2
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 3
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 4
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 5
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 6
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 7
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 8
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 9
+                question:
+                options:
+                answer:
+            },
+            {
+                // Question 10
                 question:
                 options:
                 answer:
