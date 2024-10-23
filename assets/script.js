20015b5e9be0d59aff6310a7c56a64eae1427587
(Philip) Luke <luke.isotta@gmail.com>
2024-10-23 13:11:49 +0000

    Add: onNextButton function to js

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const nextButton = document.getElementById("next");
     let topic = questionsArray[0].questions;
     //change shuffledQuestions to a function later
-    let shuffledQuestions = topic.sort(Math.random() - 0.5);
+    let shuffledQuestions = topic.sort(() => Math.random() - 0.5);
     let currentQuestionIndex = 0;
     let answerButtons = document.getElementById("answers-container").children;
 
@@ -34,7 +34,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Luke
     //marker 5
+    onNextButton(); //<--should maybe be submit button and next button?
+    function onNextButton() {
+        const answer = shuffledQuestions[currentQuestionIndex].answer;
+        console.log(shuffledQuestions);
+        console.log(answer);
 
+        if (document.querySelector(".selected").id == answer) {
+            console.log("correct")
+            console.log("score++")
+        } else {
+            console.log("incorrect")
+        }
+
+        currentQuestionIndex++;
+        if (currentQuestionIndex + 1 != shuffledQuestions.length) {
+            removeLastQuestion();
+        } else if (currentQuestionIndex + 1 == shuffledQuestions.length) {
+            finishedQuizScene();
+        } else {
+            console.log("Luke messed this if up");
+        }
+    }
+
+	// check which answer button has selected/choice
+	// if the button with selected is the same array value as answer
+	// 	score increases by one
+
+	// increase the current question index by 1
+
+	// check if current question index + 1 is greater than shuffled question index array
+	// 	if false,
+	// 		call(removeLastQuestion)
+	// 	else if true,
+	// 		call(finishedQuizScene)
+	// 	else,
+	// 		log an error to the console saying luke typed out the wrong pseudo code
 
     
 });
