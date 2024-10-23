8ec476ac8059fa9136c77d50fb621a52f5583706
Paulina Adams <pstafska@gmail.com>
2024-10-23 13:11:19 +0000

    Add:startQuiz function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
+document.addEventListener("DOMContentLoaded") => {
+
     //all JS code in here:
 
-    const startButton = document.getElementById("start");
-    const nextButton = document.getElementById("next");
+    const startButton = document.getElementById("start")
+    const nextButton = document.getElementById("next")
     let topic = questionsArray[0].questions;
     //change shuffledQuestions to a function later
     let shuffledQuestions = topic.sort(Math.random() - 0.5);
@@ -12,30 +12,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Paulina
     //marker 1
+    startButton.addEventListener("click", startQuiz)
+
+    function startQuiz() {
+        console.log("Started")
+        currentQuestionIndex = 0;
+        startButton.classList.add("hide")
+        currentQuestionIndex.classList.remove("hide")
+        shuffledQuestions = questions.sort(() => Math.random() - .5)
+    }
+
+}
+//Becky
+//marker 2
 
 
 
 
-    //Becky
-    //marker 2
+//Kate
+//marker 3
 
 
 
-
-    //Kate
-    //marker 3
-
-
-
-    //Ellie
-    //marker 4
+//Ellie
+//marker 4
 
 
 
-    //Luke
-    //marker 5
-
-
-    
-});
-
+//Luke
+//marker 5
+;
